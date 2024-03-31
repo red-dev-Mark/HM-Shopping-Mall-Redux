c458eeafe8edf351588f684fbe7a795b0d1446e2
Mark <redhero8830@gmail.com>
2024-04-01 01:04:27 +0900

    modified css

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -11,8 +11,12 @@ export default function ProductCard({ item }) {
     <div className="product-card" onClick={showDetail}>
       <img src={item.img} alt="" />
       {/* <div>{item?.choice === true ? "Consious Choice" : ""}</div> */}
-      <div style={{ fontWeight: "bold", fontSize: "20px" }}>{item.title}</div>
-      <div style={{ fontSize: "18px" }}>{`${item?.price.toLocaleString("ko-KR")}원`}</div>
+      <div style={{ fontWeight: "bold", fontSize: "20px", marginTop: "10px" }}>
+        {item.title}
+      </div>
+      <div style={{ fontSize: "18px" }}>{`${item?.price.toLocaleString(
+        "ko-KR"
+      )}원`}</div>
       <div>
         {item?.size.map((size) => {
           if (size !== "L") return size + " / ";
